d4f52f39ee189b8dfc7b4a4dd1644937d1a750ce
Susan Dreher <sdreher@gmail.com>
2018-10-03 15:28:47 -0400

    merge

--- static/js/src/quiz.js
+++ static/js/src/quiz.js
@@ -1,5 +1,6 @@
+/* global alert: true */
 jQuery(document).ready(function() {
-    jQuery('a.moretoggle').click(function(evt) {
+    jQuery('.focusgroups a.moretoggle').click(function(evt) {
         evt.preventDefault();
         var $elt = jQuery(evt.currentTarget);
         var $answer = $elt.nextAll('.toggleable');
@@ -9,6 +10,18 @@ jQuery(document).ready(function() {
         } else {
             $elt.html('Hide answer &#62;&#62;');
             $answer.show();
+        }
+        return false;
+    });
+
+    jQuery('.atlasti .btn-submit-section').click(function(evt) {
+        evt.preventDefault();
+        var $parent = jQuery(evt.currentTarget).parent();
+
+        if ($parent.find('input').val().trim().length < 1) {
+            
+        } else {
+            $parent().next().show();
         }
         return false;
     });
